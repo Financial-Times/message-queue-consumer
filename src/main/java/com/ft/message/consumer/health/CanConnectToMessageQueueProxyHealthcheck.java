e52d68e01a5bd5659efdaa4f59e81e3ae77b2c0b
Bernadette Varga <bernadette.varga@ft.com>
2015-10-07 15:03:13 +0300

    Add healthchecks for message queue proxy.

---
+++ src/main/java/com/ft/message/consumer/health/CanConnectToMessageQueueProxyHealthcheck.java
@@ -0,0 +1,131 @@
+package com.ft.message.consumer.health;
+
+import com.ft.message.consumer.config.HealtcheckParameters;
+import com.ft.message.consumer.config.MessageQueueConsumerConfiguration;
+import com.ft.message.consumer.proxy.model.CreateConsumerInstanceResponse;
+import com.ft.platform.dropwizard.AdvancedHealthCheck;
+import com.ft.platform.dropwizard.AdvancedResult;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+public class CanConnectToMessageQueueProxyHealthcheck extends AdvancedHealthCheck {
+
+    private static final Logger logger = LoggerFactory.getLogger(CanConnectToMessageQueueProxyHealthcheck.class);
+    private static final String groupName = "healthcheck";
+    private MessageQueueConsumerConfiguration configuration;
+    private HealtcheckParameters healtcheckParameters;
+    private Client proxyClient;
+
+    public CanConnectToMessageQueueProxyHealthcheck(final Client proxyClient, final MessageQueueConsumerConfiguration configuration,
+                                                    final HealtcheckParameters healtcheckParameters) {
+        super(healtcheckParameters.getName());
+        this.proxyClient = proxyClient;
+        this.configuration = configuration;
+        this.healtcheckParameters = healtcheckParameters;
+    }
+
+    @Override
+    protected AdvancedResult checkAdvanced() throws Exception {
+        ClientResponse clientResponse = null;
+        try {
+            URI uri = buildConsumerUri();
+            clientResponse = getClientResponseForProxyConnection(uri);
+            if (clientResponse.getStatus() != 200) {
+                return reportUnhealthy(String.format("Unable to connect to queue proxy. %d", clientResponse.getStatus()));
+            }
+
+            URI consumerInstance = clientResponse.getEntity(CreateConsumerInstanceResponse.class).getBaseUri();
+            URI messageReaderUri = buildMessageReaderUri(consumerInstance);
+            clientResponse = getClientResponseForMessageConsumer(messageReaderUri);
+            if (clientResponse.getStatus() != 200) {
+                return reportUnhealthy(String.format("Unable to consume messages. Proxy returned %d", clientResponse.getStatus()));
+            }
+
+            clientResponse = deleteConsumerInstance(consumerInstance);
+            if (clientResponse.getStatus() != 204) {
+                return reportUnhealthy(String.format("Unable to destroy consumer instance. Proxy returned %d", clientResponse.getStatus()));
+            }
+        } catch (Throwable ex) {
+            String message = getName() + ": " + "Exception during connecting to kafka proxy: " + ex.getLocalizedMessage();
+            return reportUnhealthy(message);
+        } finally {
+            closeClientResponse(clientResponse);
+        }
+        return AdvancedResult.healthy("OK");
+    }
+
+    private void closeClientResponse(ClientResponse clientResponse) {
+        if (clientResponse != null) {
+            clientResponse.close();
+        }
+    }
+
+    protected ClientResponse deleteConsumerInstance(URI uri) {
+        return proxyClient.resource(uri)
+                .header("Host", configuration.getQueue())
+                .delete(ClientResponse.class);
+    }
+
+    protected ClientResponse getClientResponseForMessageConsumer(URI readMessageFromUri) {
+        return proxyClient.resource(readMessageFromUri)
+                .header("Host", configuration.getQueue())
+                .header("Accept", "application/json")
+                .get(ClientResponse.class);
+    }
+
+    protected ClientResponse getClientResponseForProxyConnection(URI uri) {
+        return proxyClient.resource(uri)
+                .header("Content-Type", "application/json")
+                .header("Host", configuration.getQueue())
+                .post(ClientResponse.class);
+    }
+
+    private URI buildMessageReaderUri(URI consumerUri) {
+        return UriBuilder.fromUri(consumerUri)
+                .path("topics")
+                .path(configuration.getTopicName())
+                .build();
+    }
+
+    private URI buildConsumerUri() {
+        return UriBuilder.fromUri(configuration.getQueueProxyHost())
+                .path("consumers")
+                .path(groupName)
+                .build();
+    }
+
+    private AdvancedResult reportUnhealthy(String message) {
+        logger.warn(getName() + ": " + message);
+        return AdvancedResult.error(this, message);
+    }
+
+    @Override
+    protected int severity() {
+        return healtcheckParameters.getSeverity();
+    }
+
+    @Override
+    protected String businessImpact() {
+        return healtcheckParameters.getBusinessImpact();
+    }
+
+    @Override
+    protected String technicalSummary() {
+        return healtcheckParameters.getTechnicalSummary();
+    }
+
+    @Override
+    protected String panicGuideUrl() {
+        return healtcheckParameters.getPanicGuideUrl();
+    }
+
+    @Override
+    public String getName() {
+        return healtcheckParameters.getName();
+    }
+}
