1f99f3705bd51655b25a0cb627fb736c55a10361
Atanas Kuzmanov <atanas.kuzmanov@ft.com>
2019-11-27 19:33:17 +0200

    Added debugging information for Kafka consumer destruction.

--- src/main/java/com/ft/message/consumer/proxy/MessageQueueProxyServiceImpl.java
+++ src/main/java/com/ft/message/consumer/proxy/MessageQueueProxyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ft.message.consumer.proxy;
 
+import com.ft.message.consumer.MessageQueueConsumer;
 import com.ft.message.consumer.QueueProxyClientSingleton;
 import com.ft.message.consumer.config.MessageQueueConsumerConfiguration;
 import com.ft.message.consumer.proxy.model.CreateConsumerInstanceResponse;
@@ -12,24 +13,30 @@ import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
 import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 import java.util.List;
 
 import static com.ft.message.consumer.QueueProxyClientSingleton.*;
+import static org.apache.commons.lang.exception.ExceptionUtils.getStackTrace;
 
 public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageQueueProxyServiceImpl.class);
+
     private static final String PROXY_ERR = "Unable to %s. Proxy error.";
     private static final String PROXY_STATUS_ERR = "Unable to %s. Proxy returned %d";
     private static final String CREATE = "create consumer instance";
     private static final String CONSUME = "consume messages";
     private static final String COMMIT = "commit offsets";
     private static final String DESTROY = "destroy consumer instance";
-    
+
     private static final int SC_NO_CONTENT = ClientResponse.Status.NO_CONTENT.getStatusCode();
     private static final int SC_OK = ClientResponse.Status.OK.getStatusCode();
-    
+
     private MessageQueueConsumerConfiguration configuration;
     private Client proxyClient;
     private String status = String.format(MESSAGES_CONSUMED, 0);
@@ -37,7 +44,7 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
     private Environment env;
     private JerseyClientConfiguration jerseyConfig;
     private String queueProxyClientName;
-    
+
     public MessageQueueProxyServiceImpl(MessageQueueConsumerConfiguration configuration, Client proxyClient) {
         this.configuration = configuration;
         this.proxyClient = getQueueProxyClientSingleInstance(proxyClient, null, null, null);
@@ -55,19 +62,19 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
     }
 
     private void checkStatus(ClientResponse response, int expectedStatus, String action) {
-      if (response.getStatus() != expectedStatus) {
-        String msg = String.format(PROXY_STATUS_ERR, action, response.getStatus());
-        updateUnhealthyStatus(msg);
-        throw new QueueProxyServiceException(msg);
-      }
+        if (response.getStatus() != expectedStatus) {
+            String msg = String.format(PROXY_STATUS_ERR, action, response.getStatus());
+            updateUnhealthyStatus(msg);
+            throw new QueueProxyServiceException(msg);
+        }
     }
-    
+
     private QueueProxyServiceException proxyException(Throwable e, String action) {
-      String msg = String.format(PROXY_ERR, action);
-      updateUnhealthyStatus(msg);
-      return new QueueProxyServiceException(msg, e);
+        String msg = String.format(PROXY_ERR, action);
+        updateUnhealthyStatus(msg);
+        return new QueueProxyServiceException(msg, e);
     }
-    
+
     @Override
     public URI createConsumerInstance() {
         ClientResponse clientResponse = null;
@@ -86,7 +93,11 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
             checkStatus(clientResponse, SC_OK, CREATE);
             return clientResponse.getEntity(CreateConsumerInstanceResponse.class).getBaseUri();
         } catch (ClientHandlerException | UniformInterfaceException e) {
-          throw proxyException(e, CREATE);
+            throw proxyException(e, CREATE);
+        } catch (QueueProxyServiceException e) {
+            LOGGER.info(">>>> createConsumerInstance QueueProxyServiceException from checkStatus: " + "\n" +
+                    "exception stacktrace: " + getStackTrace(e)  + "\n");
+            throw e;
         } finally {
             if (clientResponse != null) {
                 clientResponse.close();
@@ -111,13 +122,18 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
             }
 
             clientResponse = builder.delete(ClientResponse.class);
+            LOGGER.info(">>>> checkStatus got response when trying to delete Kafka consumer instance with status: " + clientResponse.getStatus());
             checkStatus(clientResponse, SC_NO_CONTENT, DESTROY);
             updateUnhealthyStatus("Consumer has been destroyed.");
             //
             proxyClient.destroy();
             proxyClient = null;
         } catch (ClientHandlerException | UniformInterfaceException e) {
-          throw proxyException(e, DESTROY);
+            throw proxyException(e, DESTROY);
+        }  catch (QueueProxyServiceException e) {
+            LOGGER.info(">>>> destroyConsumerInstance QueueProxyServiceException from checkStatus: " + "\n" +
+                    "exception stacktrace: " + getStackTrace(e)  + "\n");
+            throw e;
         } finally {
             if (clientResponse != null) {
                 clientResponse.close();
@@ -146,11 +162,12 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
             clientResponse = builder.get(ClientResponse.class);
             checkStatus(clientResponse, SC_OK, CONSUME);
 
-            List<MessageRecord> messages = clientResponse.getEntity(new GenericType<List<MessageRecord>>() {});
+            List<MessageRecord> messages = clientResponse.getEntity(new GenericType<List<MessageRecord>>() {
+            });
             updateHealthyStatus(messages.size());
             return messages;
         } catch (ClientHandlerException | UniformInterfaceException e) {
-          throw proxyException(e, CONSUME);
+            throw proxyException(e, CONSUME);
         } finally {
             if (clientResponse != null) {
                 clientResponse.close();
@@ -177,7 +194,7 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
             clientResponse = builder.post(ClientResponse.class);
             checkStatus(clientResponse, SC_OK, COMMIT);
         } catch (ClientHandlerException | UniformInterfaceException e) {
-          throw proxyException(e, COMMIT);
+            throw proxyException(e, COMMIT);
         } finally {
             if (clientResponse != null) {
                 clientResponse.close();
@@ -193,17 +210,17 @@ public class MessageQueueProxyServiceImpl implements MessageQueueProxyService {
     private boolean queueIsNotEmpty() {
         return configuration.getQueue() != null && !configuration.getQueue().isEmpty();
     }
-    
+
     private void updateHealthyStatus(int messageCount) {
-      status = String.format(MESSAGES_CONSUMED, messageCount);
+        status = String.format(MESSAGES_CONSUMED, messageCount);
     }
-    
+
     private void updateUnhealthyStatus(String msg) {
-      status = msg;
+        status = msg;
     }
-    
+
     @Override
     public String getStatus() {
-      return status;
+        return status;
     }
 }
